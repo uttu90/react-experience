281b51143ff6599dfe98917a08a175e032460ce9
uttu90 <hondasuytu.ht@gmail.com>
2018-07-04 10:13:19 +0700

    add intersection observer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import Projects from './containers/Projects';
 
+
 import Nav from './components/nav';
 
 class App extends Component {
